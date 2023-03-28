2274aabf0f6a96f32bdb1066652561926a5edbb5
GilangDiraPrasetya <gilangdira123@gmail.com>
2023-03-29 06:55:32 +0700

    First Commit

---
+++ Array/Searching.js
@@ -0,0 +1,23 @@
+// const numbers = [9, 10, 11, 12, 8, 13];
+
+// function getBigNumber(number) {
+//   if (number > 8) {
+//     return number;
+//   }
+// }
+
+// const result = numbers.find(getBigNumber);
+
+// console.log(result); // 11
+
+const updateDinoName = (name) => {
+  // name = 't-rex';
+  // return name;
+  console.log(name);
+};
+
+let dinoName = 'brachio';
+
+window.updateDinoName(dinoName); // call function
+
+// console.log(updateDinoName(dinoName));
